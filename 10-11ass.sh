9afa40d2d388bffa9651034cd084d37da936b7d9
Shashi <shashihghebad3@gmail.com>
2021-02-17 11:52:40 -0800

    changed 10-11ass.sh

--- 10-11ass.sh
+++ 10-11ass.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
-declare -A DICTIONARY
-declare -a ARRAY
+#! /bin/bash -x
+declare -A dictionary
+declare -a array
 read -p "Number" num1
 read -p "Number" num2
 read -p "Number" num3
@@ -8,8 +8,18 @@ arithematic=$((num1+num2*num3))
 arithematic2=$((num1*num2+num3))
 arithematic3=$((num1+num2/num3))
 arithematic4=$((num1%num2+num3))
-	DICTIONARY=$(( [$arithematic] [$arithematic2] [$arithematic3] [$arithematic4] ))
-	ARRAY=$(( ${#DICTIONARY[@]} ))
-sort ARRAY -nr
-	echo "${ARRAY[@]}"
+	dictionary=( ["$arithematic"] ["$arithematic2"] ["$arithematic3"] ["$arithematic4"]
+	array=$(( ${#dictionary[@]} ))
 
+for (( i=0; i< $array; i++))
+do
+for (( j=$i; j< $array; j++))
+do
+if [${array[$i]} -lt ${array[$j]} ]
+then
+t=${array[$i]}
+array[$i]=${array[$j]}
+array[$j]=$t
+fi
+done
+done
