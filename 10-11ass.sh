974846c6901ea55c7320af08d6f5aa4aff06c935
Shashi <shashihghebad3@gmail.com>
2021-02-17 10:50:06 -0800

    changed 10-11ass.sh

--- 10-11ass.sh
+++ 10-11ass.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
+declare -A DICTIONARY
 read -p "Number" num1
 read -p "Number" num2
 read -p "Number" num3
-arithematic=$((num1+num2*num3))
+arithematic=$((num1+num2*num3)) 
 arithematic2=$((num1*num2+num3))
 arithematic3=$((num1+num2/num3))
 arithematic4=$((num1%num2+num3))
-
+	DICTIONARY=( [$arithematic] [$arithematic2] [$arithematic3] [$arithematic4]
